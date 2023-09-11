89a2a028776807351b30f3ef7be8c25b6d164e21
DanteMoore <144177717+DanteMoore@users.noreply.github.com>
2023-09-11 12:18:53 -0500

    Update SQLite.sql

--- SQLite.sql
+++ SQLite.sql
@@ -1,63 +1,63 @@
-CREATE TABLE applestore_description_combined as 
+CREATE TABLE applestore_description_combined AS 
 
-select * FROm appleStore_description1
+SELECT * FROM appleStore_description1
 
-UNion all 
+UNION ALL 
 
-select * from appleStore_description2
+SELECT * FROM appleStore_description2
 
-union all 
+UNION ALL 
 
-select * from appleStore_description3
+SELECT * FROM appleStore_description3
 
-union ALL
+UNION ALL
 
-select * from appleStore_description4
+SELECT * FROM appleStore_description4
 
 **EXPLORATORY DATA ANALYSIS**
 
 --check the number of unique apps in both tablesApplestore
 
-select count(distinct id) AS UniqueAppIDs
+SELECT COUNT(DISTINCT id) AS UniqueAppIDs
 FROM AppleStore
 
-select count(distinct id) AS UniqueAppIDs
+SELECT COUNT(DISTINCT id) AS UniqueAppIDs
 FROM applestore_description_combined
 
 --check for any missing values
 
-select count(*) MissingValues
+SELECT COUNT(*) MissingValues
 FROM AppleStore
-WHERE track_name is NULL OR user_rating IS NULL OR prime_genre is NULL
+WHERE track_name IS NULL OR user_rating IS NULL OR prime_genre IS NULL
 
-select count(*) MissingValues
+SELECT COUNT(*) MissingValues
 FROM applestore_description_combined
-WHERE app_desc is NULL
+WHERE app_desc IS NULL
 
 --find out the number of apps per genre
 
-select prime_genre, COUNT(*) AS NumApps
+SELECT prime_genre, COUNT(*) AS NumApps
 FROM AppleStore
 GROUP BY prime_genre
 ORDER BY NumApps DESC
 
 --get an overview of the apps ratings
 
-select min(user_rating) as MinRating,
+SELECT min(user_rating) AS MinRating,
        max(user_rating) AS MaxRating,
        avg(user_rating) AS AvgRating
 FROM AppleStore
 
 --get the distribution of app prices
 
-select
+SELECT
    (price / 2) AS PriceBinStart,
    ((price / 2) *2) +2 AS PriceBinEnd,
-    COunt(*) AS NumApps
+    COUNT(*) AS NumApps
 FROM AppleStore
     
-GROup by PriceBinStart
-Order by PriceBinStart
+GROUP BY PriceBinStart
+Order BY PriceBinStart
 
 ** DATA ANALYSIS**
 
@@ -69,42 +69,42 @@ SELECT CASE
          END AS App_Type,
          avg(user_rating) AS Avg_Rating
 FROM AppleStore
-Group By App_Type
+Group BY App_Type
 
 --check if apps with more supported langauges have higher ratings
 
-SElect case 
-            when lang_num < 10 Then '<10 languages'
-            when lang_num BETWEEN 10 and 30 then '10-30 languages'
+SELECT CASE 
+            WHEN lang_num < 10 THEN '<10 languages'
+            WHEN lang_num BETWEEN 10 AND 30 THEN '10-30 languages'
             ELSE '>30 languages'
          END AS language_bucket,
-         avg(user_rating) as Avg_Rating
-from AppleStore
-group by language_bucket
-order by Avg_Rating DESC
+         avg(user_rating) AS Avg_Rating
+FROM AppleStore
+GROUP BY language_bucket
+ORDER BY Avg_Rating DESC
 
 --check genres with low ratings
 
-Select prime_genre,
-       avg(user_rating) As AVg_Rating
+SELECT prime_genre,
+       avg(user_rating) AS AVg_Rating
 FROM AppleStore
 GROUP BY prime_genre
-order by Avg_Rating ASC
-limit 10
+ORDER BY Avg_Rating ASC
+LIMIT 10
 
 --check if there is correlation between the length of the app description and the user rating
 
-SELECT case
-       when length(b.app_desc) <500 THEn 'Short'
-       when length(b.app_desc) between 500 and 1000 then 'Medium'
-       else 'Long'
-    ENd As description_length_bucket,
+SELECT CASE
+       WHEN length(b.app_desc) <500 THEN 'Short'
+       WHEN length(b.app_desc) BETWEEN 500 AND 1000 THEN 'Medium'
+       ELSE 'Long'
+    END AS description_length_bucket,
     avg(a.user_rating) AS average_rating
-from AppleStore as A
-join applestore_description_combined as b
-On a.id = b.id
-group by description_length_bucket
-order by average_rating desc
+FROM AppleStore AS A
+JOIN applestore_description_combined AS b
+ON a.id = b.id
+GROUP BY description_length_bucket
+ORDER average_rating DESC
 
 --check the top rated apps for each genre 
 
@@ -112,14 +112,14 @@ SELECT
    prime_genre,
    track_name,
    user_rating
-from (
-      select 
+FROM (
+      SELECT 
       prime_genre,
       track_name,
       user_rating,
-      RANK() OVER(PARTITION by prime_genre order by user_rating desc, rating_count_tot desc) AS rank
+      RANK() OVER(PARTITION BY prime_genre ORDER BY user_rating DESC, rating_count_tot DESC) AS rank
       FROM 
       AppleStore
      ) AS a
-where 
+WHERE 
 a.rank = 1
